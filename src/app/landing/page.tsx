bdc3cd67afc4e1ff6a3ec00e422ee39133d2e167
Arijeet Das <arijeetdas999@gmail.com>
2025-04-14 19:59:56 +0530

    image change

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -101,7 +101,7 @@ export default function LandingPage() {
                                     <Card className="hover:shadow-xl transition-all duration-300 ease-in-out border-0 shadow-md overflow-hidden h-full">
                                         <div className="h-48 overflow-hidden relative">
                                             <img 
-                                                src={saloon.image || "https://images.unsplash.com/photo-1616226384899-f9890b13d852?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhcmJlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"} 
+                                                src={saloon.image || "https://media.istockphoto.com/id/639607852/photo/hairstylist-serving-client-at-barber-shop.jpg?s=612x612&w=0&k=20&c=-kBoMs26KIX1Hl6uh_VLRHCtLxnLYyq9a0n7X8iu5MQ="} 
                                                 alt={saloon.shopName} 
                                                 className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
                                             />
